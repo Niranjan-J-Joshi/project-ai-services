6369fec3919a579c1dfce4938335fb3795894b97
Niranjan Joshi <Niranjan.Joshi1@ibm.com>
2026-06-04 13:11:11 +0530

    fix : correct reset column visibitity functionality
    Signed-off-by: Niranjan Joshi <Niranjan.Joshi1@ibm.com>

--- ui/catalog/src/components/DeployedServicesTable/types.ts
+++ ui/catalog/src/components/DeployedServicesTable/types.ts
@@ -291,6 +291,7 @@ export const appReducer = (state: AppState, action: AppAction): AppState => {
           status: true,
           uptime: true,
           messages: true,
+          service:true,
         },
       };
     case ACTION_TYPES.SHOW_EXPORT_TOAST:
