41181cf538b7627d6692de8b8add8131012039e8
Niranjan Joshi <Niranjan.Joshi1@ibm.com>
2026-06-16 00:58:44 +0530

    Update supporting files
    Signed-off-by: Niranjan Joshi <Niranjan.Joshi1@ibm.com>

--- ui/catalog/vite.config.ts
+++ ui/catalog/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://catalog-api.10.48.64.152.nip.io",
+        target: "http://localhost:8080",
         changeOrigin: true,
         secure: false,
       },
